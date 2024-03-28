089da5862c2040df57ebba2d090930083575fe1e
sleepyfor <darkorst@skiff.com>
2024-03-27 23:41:36 -0700

    An Indoor Pool

---
+++ Modules/Customs/AnIndoorPool.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DarkRoles.Modules.Customs
+{
+    internal class AnIndoorPool
+    {
+        public static string IndoorPool = "An Indoor Pool. - A suggestion by glitzy";
+    }
+}
